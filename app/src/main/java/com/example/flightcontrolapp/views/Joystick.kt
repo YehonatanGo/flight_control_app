3d38fe6040ceff62c88545bdac94a55bf89b7516
Yehonatan <yehonatan.goldfarb@gmail.com>
2021-06-22 00:57:21 +0300

    fixed joystick movement

--- app/src/main/java/com/example/flightcontrolapp/views/Joystick.kt
+++ app/src/main/java/com/example/flightcontrolapp/views/Joystick.kt
@@ -23,16 +23,17 @@ class Joystick @JvmOverloads constructor(
     }
     private var radius: Float = 0f
     private var center: PointF = PointF()
-    private var orig_center: PointF = PointF()
+    private var orig_center: PointF = PointF(center.x, center.y)
+    private var x_on_down:Float = 0f
+    private var y_on_down:Float = 0f
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        radius = 0.3f* min(width,height)
+        radius = 0.2f* min(width,height)
         center = PointF(width/2.0f, height/2.0f)
+        orig_center = PointF(center.x, center.y)
     }
 
     override fun onDraw(canvas: Canvas) {
-//        super.onDraw(canvas)
-//        paint.color = Color.BLACK
         canvas.drawCircle(center.x, center.y, radius, paint)
     }
 
@@ -41,52 +42,34 @@ class Joystick @JvmOverloads constructor(
         invalidate()
     }
 
-    private var mVelocityTracker: VelocityTracker? = null
+
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
-         var x_on_down:Float = 0f
-         var y_on_down:Float= 0f
 
         when (event.actionMasked) {
             MotionEvent.ACTION_DOWN -> {
-                // Reset the velocity tracker back to its initial state.
-                mVelocityTracker?.clear()
-                // If necessary retrieve a new VelocityTracker object to watch the
-                // velocity of a motion.
-                mVelocityTracker = mVelocityTracker ?: VelocityTracker.obtain()
-                // Add a user's movement to the tracker.
-                mVelocityTracker?.addMovement(event)
-
-                x_on_down = getX()
-                y_on_down = getY()
+                x_on_down = event.getX()
+                y_on_down = event.getY()
                 Log.d("", "X on_down: ${x_on_down}")
                 Log.d("", "Y on_down: ${y_on_down}")
 
             }
             MotionEvent.ACTION_MOVE -> {
-                mVelocityTracker?.apply {
-                    val pointerId: Int = event.getPointerId(event.actionIndex)
-                    addMovement(event)
-                    // When you want to determine the velocity, call
-                    // computeCurrentVelocity(). Then call getXVelocity()
-                    // and getYVelocity() to retrieve the velocity for each pointer ID.
-                    computeCurrentVelocity(1000)
-                    // Log velocity of pixels per second
-                    // Best practice to use VelocityTrackerCompat where possible.
-                    Log.d("", "X velocity: ${x_on_down}")
-                    Log.d("", "Y velocity: ${y_on_down}")
+//                    Log.d("", "X velocity: ${x_on_down}")
+//                    Log.d("", "Y velocity: ${y_on_down}")
+                    Log.d("", "getX: ${event.getX()}")
+                    Log.d("", "getY: ${event.getY()}")
                     center.x += event.getX() - x_on_down
                     center.y += event.getY() - y_on_down
+                    x_on_down = event.getX()
+                    y_on_down = event.getY()
                     invalidate()
                 }
-            }
+
             MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-                // Return a VelocityTracker object back to be re-used by others.
-                mVelocityTracker?.recycle()
-                mVelocityTracker = null
-                center = orig_center
+                center = PointF(orig_center.x,orig_center.y)
                 Log.d("", "X up: ${x_on_down}")
-                Log.d("", "Y down: ${y_on_down}")
+                Log.d("", "Y up: ${y_on_down}")
                 invalidate()
             }
         }
