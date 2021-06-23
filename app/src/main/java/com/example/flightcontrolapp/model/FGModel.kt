b2945c46120a23bc6b02946b7ee19f89d377dcb1
yanirbuznah <65356872+yanirbuznah@users.noreply.github.com>
2021-06-23 16:53:48 +0300

    add executor and things start to work

--- app/src/main/java/com/example/flightcontrolapp/model/FGModel.kt
+++ app/src/main/java/com/example/flightcontrolapp/model/FGModel.kt
@@ -3,57 +3,62 @@ package com.example.flightcontrolapp.model
 import android.util.Log
 import java.io.PrintWriter
 import java.io.Writer
-import java.lang.Thread.sleep
 import java.net.Socket
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 import kotlin.concurrent.thread
+
+
+
+
 
 class FGModel {
     private lateinit var socket : Socket
     private lateinit var out : Writer
+    private var executor : Executor = Executors.newSingleThreadExecutor()
+
+
+    class SendMessage(private var out: Writer, private var message: String) : Runnable{
+
+        override fun run() {
+            out.write(message)
+            out.flush()
+        }
+
+    }
     fun setAileron(new_aileron:Float){
         /*
          TODO: make sure value is not bigger than 1 or smaller than -1 can
          */
 
         Log.d("", "new_aileron:${new_aileron}")
-        thread {
-            out.write("set /controls/flight/aileron "+new_aileron+"\r\n")
-            out.flush()
-        }
+        //aileron.sendAileron(new_aileron)
+        executor.execute(SendMessage(out,"set /controls/flight/aileron "+new_aileron+"\r\n"))
 
     }
 
     fun setElevator(new_elevator:Float){
         Log.d("", "new_elevator:${new_elevator}\n")
-        thread {
-            out.write("set /controls/flight/elevator "+new_elevator+"\r\n")
-            out.flush()
-        }
+        executor.execute(SendMessage(out,"set /controls/flight/elevator "+new_elevator+"\r\n"))
 
     }
 
     fun setRudder(new_rudder: Float){
-        thread {
-            out.write("set /controls/flight/rudder "+new_rudder+"\r\n")
-            out.flush()
-        }
+        executor.execute(SendMessage(out,"set /controls/flight/elevator "+new_rudder+"\r\n"))
 
     }
 
     fun setThrottle(new_throttle:Float){
-        thread {
-            out.write("set /controls/engines/current-engine/throttle " +new_throttle+"\r\n")
-            out.flush()
-        }
+        executor.execute(SendMessage(out,"set /controls/engines/current-engine/throttle "+new_throttle+"\r\n"))
 
     }
 
     fun connect(ip:String, port:Int){
         Log.d("","IP: ${ip}, Port: ${port}")
-        thread {
+        executor.execute(Runnable {
             socket = Socket(ip,port)
             out = PrintWriter(socket.getOutputStream(),true)
-        }
+        })
 
     }
 
